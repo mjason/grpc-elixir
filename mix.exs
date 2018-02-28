6b3507a95fef085da43529ab4054ccd6ca0dd8eb
mj <tywf91@gmail.com>
2018-02-28 16:08:00 +0800

    only client

--- mix.exs
+++ mix.exs
@@ -39,7 +39,6 @@ defmodule GRPC.Mixfile do
       # https://github.com/joedevivo/chatterbox/pull/114
       # https://github.com/joedevivo/chatterbox/pull/115
       {:chatterbox, github: "tony612/chatterbox", branch: "my-fix"},
-      {:cowboy, "~> 2.2.0"},
       {:ex_doc, "~> 0.14", only: :dev},
       {:inch_ex, ">= 0.0.0", only: :docs},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false}
